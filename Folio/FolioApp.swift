ce0649976c0682c0a98556e4483e5c97bca5e9c2
DeweyDripp <deweydrippmusic@gmail.com>
2026-09-10 19:09:38 -0500

    Initial Commit

---
+++ Folio/FolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolioApp.swift
+//  Folio
+//
+//  Created by Ryan Ramirez on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FolioTests/FolioTests.swift
@@ -0,0 +1,19 @@
+//
+//  FolioTests.swift
+//  FolioTests
+//
+//  Created by Ryan Ramirez on 9/10/26.
+//
+
+import Testing
+@testable import Folio
+
+struct FolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
